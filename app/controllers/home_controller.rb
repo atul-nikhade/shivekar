e946202679d98d5693e0680188825edf2e1858d1
atul-nikhade <atsnikhade@gmail.com>
2016-10-05 00:32:20 +0530

    remove all unwanted files

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -3,6 +3,9 @@ class HomeController < ApplicationController
   def index
   end
 
+  def contact_us
+  end	
+
   def send_mail
   	UserResponseMailer.user_response(params).deliver
     # flash[:success] = 'Your message sent successfully to Atul. Thanks for your message. Keep in touch'
